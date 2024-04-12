3a2fafce546a886c33ba5158591c6c1b23cfe6e3
zammsara <saztaylor@uamv.edu.ni>
2024-04-12 08:40:14 -0600

    ejercicios

---
+++ numerosPares.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int numbers [5];
+    numbers[0]=2;
+    numbers[1]=44;
+    numbers[2]=106;
+    numbers[3]=1000;
+    numbers[4]=202;
+    printf("El elemento # 1 %d\n", numbers[0]);
+    printf("El elemento # 2 %d\n", numbers[1]);
+    printf("El elemento # 3 %d\n", numbers[2]);
+    printf("El elemento # 4 %d\n", numbers[3]);
+    printf("El elemento # 5 %d\n", numbers[4]);
+    
+    
+    
+    return 0;
+}
