ea3233bd56c9e2f619c725cb76e044d4ac2f329d
zammsara <saztaylor@uamv.edu.ni>
2024-06-28 09:03:32 -0600

    28 Junio

---
+++ 28 JUN/principal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <locale.h>
+#include "funciones.cpp"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    setlocale(LC_ALL, "spanish");
+    principal();
+    return 0;
+}
